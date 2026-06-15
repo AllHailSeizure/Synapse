ad534fc2a94b2567507b4f6ca4a0ff595e344470
AllHailSeizure <natebosma@gmail.com>
2026-06-15 00:11:31 -0400

    feat: register Tile Select tool with click-to-select-cell

--- Pixelorama/TileTools/Main.gd
+++ Pixelorama/TileTools/Main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var extension_api: Node
+const TOOL_NAME := "TileSelect"
 
 
 func _enter_tree() -> void:
@@ -8,8 +9,20 @@ func _enter_tree() -> void:
 	if extension_api == null:
 		push_error("TileTools: ExtensionsApi not found")
 		return
+	var tool_scene := preload("res://TileSelectTool.tscn")
+	extension_api.add_tool(
+		TOOL_NAME,
+		"Tile Select",
+		tool_scene,
+		[],
+		"Grid-based tile selection",
+		"",
+		[],
+		-1
+	)
 
 
 func _exit_tree() -> void:
 	if extension_api == null:
 		return
+	extension_api.remove_tool(TOOL_NAME)
--- Pixelorama/TileTools/TileSelectTool.gd
+++ Pixelorama/TileTools/TileSelectTool.gd
@@ -2,6 +2,10 @@ extends Node
 
 var grid_size := Vector2i(20, 20)
 
+var _selected_cells: Array[Vector2i] = []
+var _is_drawing := false
+var _is_shift_held := false
+
 
 func pos_to_cell(pos: Vector2i) -> Vector2i:
 	var cell_x := pos.x / grid_size.x
@@ -33,3 +37,57 @@ func cells_bounding_box(cells: Array[Vector2i]) -> Rect2i:
 func snap_to_grid(pos: Vector2i) -> Vector2i:
 	var cell := pos_to_cell(pos)
 	return cell * grid_size
+
+
+func draw_start(pos: Vector2i) -> void:
+	_is_drawing = true
+	_is_shift_held = Input.is_key_pressed(KEY_SHIFT)
+	var cell := pos_to_cell(pos)
+	if not _is_shift_held:
+		_selected_cells.clear()
+		_clear_selection()
+	if cell not in _selected_cells:
+		_selected_cells.append(cell)
+	_apply_selection()
+
+
+func draw_move(pos: Vector2i) -> void:
+	if not _is_drawing:
+		return
+	var cell := pos_to_cell(pos)
+	if cell not in _selected_cells:
+		_selected_cells.append(cell)
+		_apply_selection()
+
+
+func draw_end(pos: Vector2i) -> void:
+	_is_drawing = false
+
+
+func cursor_move(pos: Vector2i) -> void:
+	pass
+
+
+func draw_indicator(left: bool) -> void:
+	pass
+
+
+func draw_preview() -> void:
+	pass
+
+
+func _apply_selection() -> void:
+	var ext_api := get_node_or_null("/root/ExtensionsApi")
+	if ext_api == null:
+		return
+	ext_api.selection.clear_selection()
+	for cell in _selected_cells:
+		var rect := cell_to_rect(cell)
+		ext_api.selection.select_rect(rect, 0)  # 0 = add
+
+
+func _clear_selection() -> void:
+	var ext_api := get_node_or_null("/root/ExtensionsApi")
+	if ext_api == null:
+		return
+	ext_api.selection.clear_selection()
