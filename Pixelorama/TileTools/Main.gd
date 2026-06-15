df7bb73fe2e6933bc78f2bdfe79b2737e4bd02b7
AllHailSeizure <natebosma@gmail.com>
2026-06-15 00:13:36 -0400

    feat: add grid overlay that shows when Tile Select is active

--- Pixelorama/TileTools/Main.gd
+++ Pixelorama/TileTools/Main.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var extension_api: Node
+var grid_overlay: Node2D
 const TOOL_NAME := "TileSelect"
 
 
@@ -26,3 +27,19 @@ func _exit_tree() -> void:
 	if extension_api == null:
 		return
 	extension_api.remove_tool(TOOL_NAME)
+
+
+func show_grid_overlay() -> void:
+	if grid_overlay != null:
+		return
+	grid_overlay = preload("res://GridOverlay.gd").new()
+	grid_overlay.name = "TileToolsGridOverlay"
+	var canvas := extension_api.general.get_canvas()
+	if canvas != null:
+		canvas.add_child(grid_overlay)
+
+
+func hide_grid_overlay() -> void:
+	if grid_overlay != null:
+		grid_overlay.queue_free()
+		grid_overlay = null
